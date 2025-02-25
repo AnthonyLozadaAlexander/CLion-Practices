df1f8450f19574059dd60987b10eae10973a1b77
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-24 22:04:47 -0500

    add Ejercicio13 source file and update Ejercicio12 to calculate the sum of squares using a while loop

--- Ejercicio13.cpp
+++ Ejercicio13.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 
 int main() {
-    // code: Realice un programa que calcule y muestre en la salida estandar la suma de los cuadrados de los 10 primeros enteros mayores que cero
+    // code
+    int num = 0, i;
 
-    int num = 0, i = 1;
-    while (i <= 10) {
+    for (i = 1; i <= 10; i++) {
         num = num + pow(i, 2);
-        i = i + 1;
     }
 
     std::cout << "La suma de los cuadrados de los 10 primeros numeros enteros es: " << num << std::endl;
+
     return 0;
 }
