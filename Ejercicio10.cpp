dfb25b2c198ad8d0e07c2dfaa4f8415c34c9caed
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-24 20:03:51 -0500

    add Ejercicio10 source file with reverse counting functionality

---
+++ Ejercicio10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // code: While inverso de 100 hasta llegar a 1
+    int i = 100;
+
+    while (i >= 1) {
+        std::cout << "numero -> " << i << std::endl;
+        i = i - 1;
+    }
+
+    return 0;
+}
