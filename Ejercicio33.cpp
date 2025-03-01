cb45c680f8f37969ab8cd6ff48875adb12969481
zDefcon20 <antholozadarequelme@gmail.com>
2025-02-28 19:38:11 -0500

    add Ejercicio33 source file to implement a program that calculates the sum of an array

---
+++ Ejercicio33.cpp
@@ -0,0 +1,22 @@
+//
+// Created by zDefcon on 28/02/2025.
+//
+#include <iostream>
+
+int main() {
+    // code
+    int num[] = {2, 4, 6, 8, 10};
+    int suma = 0;
+
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "\tSuma De Un Array" << std::endl;
+    std::cout << "--------------------------------\n" << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "num["<<i<<"] = " << num[i] << std::endl;
+        std::cout << num[i] << " + " << suma << std::endl;
+        suma = suma + num[i];
+        std::cout << " = " << suma << std::endl;
+    }
+    return 0;
+}
