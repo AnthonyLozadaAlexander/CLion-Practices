34f548137d0d1968454ebb850289f746ca3788e2
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-01 23:55:49 -0500

    add Ejercicio38 source file to implement a program for user-defined array input, find the maximum and minimum elements

---
+++ Ejercicio38.cpp
@@ -0,0 +1,32 @@
+//
+// Created by zDefcon on 01/03/2025.
+//
+#include <iostream>
+
+int main() {
+    // code
+    int nums[] = {4,5,2,7,1}, mayor, menor;
+
+    mayor = nums[0];
+    menor = nums[0];
+
+    std::cout << "---------------------------" << std::endl;
+    std::cout << "Elementos Del Arreglo: " << std::endl;
+    for (int i = 0; i < 5; i++) {
+
+        std::cout << "nums["<<i<<"] -> " << nums[i] << std::endl;
+
+        if (nums[i] > mayor) {
+            mayor = nums[i];
+        }
+        if (nums[i] < menor) {
+            menor = nums[i];
+        }
+    }
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "El Elemento Mayor Del Arreglo Es: " << << mayor << std::endl;
+    std::cout << "El Elemento Menor Del Arreglo Es: " << menor << std::endl;
+
+    system("pause");
+    return 0;
+}
