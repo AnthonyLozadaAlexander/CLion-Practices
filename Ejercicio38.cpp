1dc4628225fab7978f82b142a099a41f1243b033
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-02 00:15:38 -0500

    fix Ejercicio38 to display indices of maximum and minimum elements in the array

--- Ejercicio38.cpp
+++ Ejercicio38.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     // code
-    int nums[] = {4,5,2,7,1}, mayor, menor;
+    int nums[] = {4,5,2,7,1}, mayor, menor, indiceMayor = 0, indiceMenor=0;
 
     mayor = nums[0];
     menor = nums[0];
@@ -18,18 +18,20 @@ int main() {
 
         if (nums[i] > mayor) {
             mayor = nums[i];
+            indiceMayor = i;
         }
         if (i == 5-1) {
             std::cout << "------------------------------------" << std::endl;
-            std::cout << "El Elemento Mayor Del Arreglo Es: " << mayor << std::endl;
+            std::cout << "El Elemento Mayor Del Arreglo Es: " <<"nums["<<indiceMayor<<"] = "<< mayor << std::endl;
 
         }
         if (nums[i] < menor) {
             menor = nums[i];
+            indiceMenor = i;
         }
         if (i == 5-1) {
 
-            std::cout << "El Elemento Menor Del Arreglo Es: " << menor << std::endl;
+            std::cout << "El Elemento Menor Del Arreglo Es: " <<"nums["<<indiceMenor<<"] = " << menor << std::endl;
             std::cout << "------------------------------------" << std::endl;
         }
     }
