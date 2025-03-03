f232760dd4db9ac63e14570ae7c00734bca5a72d
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-03 01:27:38 -0500

    add Ejercicio42 source file to implement a program that prompts for the number of grades to average and calculates the average

--- Ejercicio42.cpp
+++ Ejercicio42.cpp
@@ -6,20 +6,31 @@
 int main() {
     // code
     int n=0;
-    double nota = 0, promedio=0;
+    double ca = 0, promedio=0, acumulador = 0;
 
     do{
+        std::cout << "\n";
         std::cout << "Ingrese la cantidad de notas a promediar" << std::endl;
-        std::cout << "- >"; std::cin >> n;
+        std::cout << "-> "; std::cin >> n;
 
     if(n <= 0){
-      std::cout << "Error: la cantidad de notas debe ser mayor a 0" << std::endl;
+      std::cout << "\nError: la cantidad de notas debe ser mayor a 0" << std::endl;
     }
     else{
 
-      std::cout << "Cantidad Ingresada Correctamente" << std::endl;
+      std::cout << "\nCantidad Ingresada Correctamente" << std::endl;
     }
     }while(n <= 0);
 
+    for (int i = 1; i <= n; i++) {
+        std::cout << "-------------------------------------" << std::endl;
+        std::cout << "Ingrese la nota No " << i << std::endl;
+        std::cin >> ca;
+        acumulador = acumulador + ca;
+        std::cout << "------------------------------------" << std::endl;
+    }
+
+    promedio = acumulador / n;
+    std::cout << "El promedio de las " << n << " notas ingresadas es: " << promedio << std::endl;
     return 0;
 }
