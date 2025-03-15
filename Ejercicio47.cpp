1eb7146ee4166192deb768c321aa1ad7d852e99b
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-15 02:17:02 -0500

    add Ejercicio47 source file to implement a program that captures user input for a 2x2 matrix and displays its elements along with a copy of the matrix

--- Ejercicio47.cpp
+++ Ejercicio47.cpp
@@ -7,8 +7,9 @@
 int main() {
     // code
     int matrix[2][2]; // 2x2
+    int matrixCopy[2][2];
     std::cout << "---------------Welcome----------------" << std::endl;
-    std::cout << "Input 4 numbers to fill the matrix" << std::endl;
+    std::cout << "Input 4 numbers to fill the matrix\n" << std::endl;
     for (int i = 0; i < 2; i++) {
         std::cout << "Row["<<i<<"]" << std::endl;
         for (int j = 0; j < 2; j++) {
@@ -17,10 +18,29 @@ int main() {
         }
     }
     std::cout << "---------------------------------------" << std::endl;
-    std::cout << "Data in the matrix" << std::endl;
-    for (int i = 0; i < 2; i++) {
+    std::cout << "Data in the matrix\n" << std::endl;
+    for(int i = 0; i < 2; i++)
+    {
         for (int j = 0; j < 2; j++) {
             std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Pasando los datos de matrix a matrixCopy
+    for(int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++) {
+           matrixCopy[i][j] = matrix[i][j];
+        }
+    }
+
+    std::cout << "---------------------------------------" << std::endl;
+    std::cout << "Data in the matrixCopy\n" << std::endl;
+    for(int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++) {
+            std::cout << matrixCopy[i][j] << " ";
         }
         std::cout << std::endl;
     }
