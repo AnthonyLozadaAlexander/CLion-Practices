5986e69c0f2c0307795834b47f25ee0658a439f3
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-16 00:00:05 -0500

    add Ejercicio50 to implement user input for matrix dimensions

---
+++ Ejercicio50.cpp
@@ -0,0 +1,16 @@
+//
+// Created by zDefcon on 15/03/2025.
+//
+#include <iostream>
+#include <conio.h>
+
+int main() {
+    // code
+    int nums[20][20], filas = 0, columnas = 0;
+
+    std::cout << "Ingrese el tamaño de las filas" << std::endl;
+    std::cout << "-> "; std::cin >> filas;
+    std::cout << "Ingrese el tamaño de las columnas" << std::endl;
+    std::cout << "-> "; std::cin >> columnas;
+    return 0;
+}
