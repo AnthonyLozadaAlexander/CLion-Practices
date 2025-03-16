ec376b083b277d3ebe00c093e3d82c6dfef055b3
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-15 21:14:27 -0500

    add Ejercicio48 to implement matrix element transfer and display functionality

--- Ejercicio48.cpp
+++ Ejercicio48.cpp
@@ -13,6 +13,7 @@ int main() {
     std::cout << "Ingrese el numero de columnas" << std::endl;
     std::cout << "-> "; std::cin >> c;
 
+    int matrizCopy[f][c];
     int matriz[f][c];
 
     std::cout << "Elementos de la matriz" << std::endl;
@@ -34,5 +35,25 @@ int main() {
         }
         std::cout << std::endl;
     }
+
+    std::cout << "\n-------------------------------------" << std::endl;
+    std::cout << "Traspaso de los elementos de la matriz" << std::endl;
+    for (int i = 0; i < f; i++) {
+        std::cout << "\nRow["<<i<<"]" << std::endl;
+        for (int j = 0; j < c; j++) {
+            matrizCopy[i][j] = matriz[i][j];
+            std::cout << "Column["<<j<<"] =" << matrizCopy[i][j] << std::endl;
+        }
+    }
+
+    std::cout << "-------------------------------------" << std::endl;
+    std::cout << "MatrizCopy Grafica" << std::endl;
+    for (int i = 0; i < f; i++) { // entra a la fila primero
+        for (int j = 0; j < c;j++ ) { // recorre las columnas
+            std::cout << matrizCopy[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
