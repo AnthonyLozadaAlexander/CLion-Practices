c2143f05d85046ede40ddd32a3eaa73d16e67cb4
zDefcon20 <antholozadarequelme@gmail.com>
2025-03-15 21:32:23 -0500

    fix Ejercicio48 to correct random number range and improve output formatting

--- Ejercicio48.cpp
+++ Ejercicio48.cpp
@@ -7,7 +7,7 @@
 int main() {
     // code
     srand(time(NULL)); // creacion de randomizer
-    int f=0, c=0;
+    int f = 0, c = 0;
     std::cout << "Ingrese el numero de filas" << std::endl;
     std::cout << "-> "; std::cin >> f;
     std::cout << "Ingrese el numero de columnas" << std::endl;
@@ -20,7 +20,7 @@ int main() {
     for (int i = 0; i < f; i++) {
         std::cout << "\nRow["<<i<<"]" << std::endl;
         for (int j = 0; j < c; j++) {
-            int Random = (rand() % 20)+1; // randomizer del 1 al 20 aleatoriamente
+            int Random = (rand() % 10)+1; // randomizer del 1 al 20 aleatoriamente
             matriz[i][j] = Random;
             std::cout << "Column["<<j<<"]" << std::endl;
             std::cout << "-> " << matriz[i][j] << std::endl;
@@ -42,7 +42,7 @@ int main() {
         std::cout << "\nRow["<<i<<"]" << std::endl;
         for (int j = 0; j < c; j++) {
             matrizCopy[i][j] = matriz[i][j];
-            std::cout << "Column["<<j<<"] =" << matrizCopy[i][j] << std::endl;
+            std::cout << "Column["<<j<<"] = " << matrizCopy[i][j] << std::endl;
         }
     }
 
