ed761ccf2565cf4d74d88df040a1d5eff6340667
zDefcon <antholozadarequelme@gmail.com>
2026-01-02 02:34:41 -0500

    agrega opciones de moneda para la conversión en Ejercicio53

--- Ejercicio53.cpp
+++ Ejercicio53.cpp
@@ -8,7 +8,11 @@
     std::endl;
     std::cout << "--------------------------------------------" << std::endl;
     std::cout << "Seleccione la moneda a la que desea convertir: " << std::endl;
+    std::cout << "1 -> Euros" << std::endl;
+    std::cout << "2 -> Pesos Mexicanos" << std::endl;
     if (!(std::cin >> opcion)) return 1;
-    switch (opcion) {  }
+    switch (opcion) {
+        case 1:
+    }
     return 0;
 }
