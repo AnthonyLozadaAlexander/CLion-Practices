44c38c4eb1bb6288be1db42c317395f7316341f7
zDefcon <antholozadarequelme@gmail.com>
2026-01-02 02:43:48 -0500

    agrega funciones de conversión a diferentes monedas en Ejercicio53
    Se implementan funciones para convertir dólares a pesos mexicanos, euros y reales.
    Esto permite al usuario seleccionar la moneda a la que desea convertir la cantidad ingresada.

--- Ejercicio53.cpp
+++ Ejercicio53.cpp
@@ -1,18 +1,32 @@
-﻿int main() {
+﻿double conversionPesosMex(double dolares) {
+    return dolares * 20.50;
+}
+
+double conversionEuros(double dolares) {
+    return dolares * 0.92;
+}
+
+double conversionReales(double dolares) {
+    return dolares * 4.85;
+}
+
+
+int main() {
     double dolares = 0.0;
     int opcion = 0;
 
     std::cout << "------------------BIENVENIDO----------------" << std::endl;
     std::cout << " Ingrese la cantidad de Dolares a convertir: " << std::endl;
     if (!(std::cin >> dolares)) return 1;
-    std::endl;
     std::cout << "--------------------------------------------" << std::endl;
     std::cout << "Seleccione la moneda a la que desea convertir: " << std::endl;
-    std::cout << "1 -> Euros" << std::endl;
-    std::cout << "2 -> Pesos Mexicanos" << std::endl;
+    std::cout << "1 -> Euros\n" << "2 -> Pesos Mexicanos\n" << "3 -> Reales\n" << std::endl;
     if (!(std::cin >> opcion)) return 1;
     switch (opcion) {
         case 1:
+            std::cout << "Dolares Ingresados: $" << dolares << std::endl;
+            std::cout << "Dolares a Euros: $" << conversionPesosMex(dolares) << std::endl;
+        break;
     }
     return 0;
 }
