40b54ae81aed0d2bf60ec3980f2216437fe6c95f
zDefcon <antholozadarequelme@gmail.com>
2026-01-02 02:33:27 -0500

    agrega opción para seleccionar moneda en la conversión de dólares
    Se implementa un menú que permite al usuario ingresar la cantidad de dólares a convertir y seleccionar la moneda de destino. Se añaden validaciones para la entrada del usuario.

--- Ejercicio53.cpp
+++ Ejercicio53.cpp
@@ -1,6 +1,14 @@
 ﻿int main() {
     double dolares = 0.0;
-    std::cout << "Ingrese la cantidad de Dolares a convertir: " << std::endl;
-    std::cin >> dolares;
+    int opcion = 0;
+
+    std::cout << "------------------BIENVENIDO----------------" << std::endl;
+    std::cout << " Ingrese la cantidad de Dolares a convertir: " << std::endl;
+    if (!(std::cin >> dolares)) return 1;
+    std::endl;
+    std::cout << "--------------------------------------------" << std::endl;
+    std::cout << "Seleccione la moneda a la que desea convertir: " << std::endl;
+    if (!(std::cin >> opcion)) return 1;
+    switch (opcion) {  }
     return 0;
 }
