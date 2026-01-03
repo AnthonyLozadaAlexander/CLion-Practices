67f835aaa2d2c40424cd0d798bc12470d42d3b80
zDefcon <antholozadarequelme@gmail.com>
2026-01-02 19:43:42 -0500

    Agrega función para calcular la temperatura media de una semana
    Se implementa la función `calcularTemperaturaMedia` que recibe un arreglo de temperaturas y devuelve el promedio. También se añade la función `listarTemperaturas` para mostrar las temperaturas ingresadas. El programa principal permite al usuario ingresar las temperaturas de siete días y calcular su media.

---
+++ Ejercicio55.cpp
@@ -0,0 +1,26 @@
+﻿float calcularTemperaturaMedia(float temperaturas[]) {
+float promedio = 0.0;
+    for (int i = 0; i < 7; i++) {
+        promedio = promedio + temperaturas[i];
+    }
+    promedio = promedio / 7;
+    return promedio;
+}
+
+void listarTemperaturas(float temperaturas[]) {
+    std::cout << "Las Temperaturas Ingresadas Son: " << std::endl;
+}
+
+
+int main() {
+    float temperaturas[7];
+    int count = 0;
+    std::cout << "------------------------------------------------------------"; std::endl;
+    std::cout << " Ejercicio 55: Calcular la temperatura media de una semana";   std::endl;
+    std::cout << "------------------------------------------------------------"; std::endl;
+    while (count < 7) {
+        std::cout << "Ingrese la temperatura del dia [" << count + 1 << "] -> ";
+        std::cin >> temperaturas[count];
+        count++;
+    }
+}
